0cd6e2b66aa438d690de4e15cf21786f6beff4e1
asviananda <62512432+asviananda@users.noreply.github.com>
2020-05-16 19:40:03 +0700

    Player Movement added

---
+++ Maze Runner/Assets/Art/Animation/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Permissions;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rigidbody2D;
+
+    public Animator animator;
+
+    Vector2 vector2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+
+        vector2.x = Input.GetAxisRaw("Horizontal");
+        vector2.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", vector2.x);
+        animator.SetFloat("Vertical", vector2.y);
+        animator.SetFloat("Speed", vector2.sqrMagnitude);
+
+    }
+
+    void FixedUpdate()
+    {
+        //Movement
+
+        rigidbody2D.MovePosition(rigidbody2D.position + vector2 * moveSpeed * Time.fixedDeltaTime);
+    }    
+}
