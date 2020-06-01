412a0431a0f5fcf86e93b7ee1f055f971113372d
codestellations <erlindaargyt@gmail.com>
2020-06-01 08:52:33 +0700

    UI - Help menu
    Added pause menu
    - pause game
    - how to play

---
+++ Maze Runner/Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausePanel;
+
+    void Update(){
+        if(Input.GetMouseButtonDown(0)){
+            ResumeGame();
+        }
+    }
+
+    public void PauseGame(){
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame(){
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
